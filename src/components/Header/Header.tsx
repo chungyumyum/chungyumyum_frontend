805fbf7b2014c42084ec391e4f6e5f1f0d7f2ea8
Taejin Kim <rlaxowls1316@naver.com>
2024-04-29 23:05:50 +0900

    UI: add css to header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,22 @@
+import controlIcon from "../../assets/icons/control.svg";
+import styles from "./Header.module.css";
+import searchIcon from "../../assets/icons/search.svg";
+
+export default function Header() {
+  return (
+    <header className={styles.container}>
+      <h1 className={styles.title}>충냠냠</h1>
+      <div className={styles.searchContainer}>
+        <input className={styles.search} type="text" />
+        <img className={styles.searchIcon} src={searchIcon} alt="search_icon" />
+      </div>
+      <div>
+        <img
+          className={styles.controlIcon}
+          src={controlIcon}
+          alt="control_icon"
+        />
+      </div>
+    </header>
+  );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,9 @@
+import Header from "../components/Header";
+
 export default function Home() {
-  return <div>충냠냠</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
