189f31f7ea682f13b0eb1b8979aa74f378dfc2dd
Taejin Kim <rlaxowls1316@naver.com>
2024-04-30 14:35:56 +0900

    Feat: add temporary/banner

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,27 +1,23 @@
 import Header from "../../components/Header";
 import styles from "./Home.module.css";
+import bannerCover from "../../assets/covers/banner.svg";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Header />
       <div className={styles.contents}>
+        <div className={styles.slider}>
+          <a href="#" className={styles.banner}>
+            <img src={bannerCover} alt="banner-cover" />
+          </a>
+        </div>
         <p>foajeofw</p>
         <p>foajeofw</p>
         <p>foajeofw</p>
         <p>foajeofw</p>
         <p>foajeofw</p>
         <p>foajeofw</p>
-
-        <p>foajeofw</p>
-        <p>foajeofw</p>
-        <p>foajeofw</p>
-        <p>foajeofw</p>
-        <p>foajeofw</p>
-        <p>foajeofw</p>
-        <p>foajeofw</p>
-        <p>foajeofw</p>
-
         <p>foajeofw</p>
         <p>foajeofw</p>
         <p>foajeofw</p>
