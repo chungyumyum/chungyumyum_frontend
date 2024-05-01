74e7a4a3b970ac5f67e69a835361dcc14b3ecac4
Taejin Kim <rlaxowls1316@naver.com>
2024-05-01 21:40:28 +0900

    Feat: implement textarea

--- src/pages/post/Post.tsx
+++ src/pages/post/Post.tsx
@@ -3,8 +3,11 @@ import styles from "./Post.module.css";
 import gpsIcon from "../../assets/icons/gps.svg";
 import cameraIcon from "../../assets/icons/camera-line.svg";
 import starIcon from "../../assets/icons/star.svg";
+import { useState } from "react";
 
 export default function Post() {
+  const [des, setDes] = useState("");
+
   return (
     <div>
       <SubHeader title="글쓰기" />
@@ -28,7 +31,16 @@ export default function Post() {
               <img src={starIcon} alt="star_icon" />
             </div>
           </div>
-          <textarea className={styles.textarea}></textarea>
+          <div className={styles.textareaContainer}>
+            <textarea
+              placeholder="설명을 입력하세요."
+              className={styles.textarea}
+              onChange={(e) => setDes(e.target.value)}
+              maxLength={299}
+            ></textarea>
+            <span className={styles.wordCountTag}>{des.length}/300</span>
+          </div>
+          <button className={styles.submitBtn}>등록</button>
         </form>
       </div>
     </div>
