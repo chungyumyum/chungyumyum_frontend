9d3d1d7b17423497a7140cf72808fa699e150d8b
Taejin Kim <rlaxowls1316@naver.com>
2024-05-03 17:34:56 +0900

    Feat: implment reviewDetail's page header

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -3,12 +3,13 @@ import riceIcon from "../../assets/icons/rice.svg";
 
 type BadgeProps = {
   type: "one" | "two" | "three" | "four" | "five";
+  size?: "small" | "big";
 };
 
-export default function Badge({ type }: BadgeProps) {
+export default function Badge({ type, size = "small" }: BadgeProps) {
   return (
-    <div className={`${styles.badge} ${styles[type]}`}>
-      <img width={10} src={riceIcon} alt="rice_icon" />
+    <div className={`${styles.badge} ${styles[type]} ${styles[size]}`}>
+      <img width={size === "small" ? 10 : ""} src={riceIcon} alt="rice_icon" />
     </div>
   );
 }
--- src/pages/reviewDetail/ReviewDetail.tsx
+++ src/pages/reviewDetail/ReviewDetail.tsx
@@ -1,9 +1,32 @@
+import { Badge } from "../../components";
 import SubHeader from "../../components/SubHeader/SubHeader";
+import styles from "./ReviewDetail.module.css";
+import gpsIcon from "../../assets/icons/gps.svg";
+import bookmarkIcon from "../../assets/icons/bookmark.svg";
 
 export default function ReviewDetail() {
   return (
-    <div>
+    <>
       <SubHeader title="행보케" />
-    </div>
+      <div className={styles.contents}>
+        <div className={styles.header}>
+          <div className={styles.profile}>
+            <Badge type="three" size="big" />
+            <div>
+              <p className={styles.profileTitle}>김태진</p>
+              <p className={styles.profileRole}>쩝쩝석사</p>
+            </div>
+          </div>
+          <div>
+            <button className={styles.gpsBtn}>
+              <img src={gpsIcon} alt="gps_icon" />
+            </button>
+            <button className={styles.bookmarkBtn}>
+              <img src={bookmarkIcon} alt="gps_icon" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
