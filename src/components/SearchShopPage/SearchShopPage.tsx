6ce4fe7949179173d877658ab0b84b8f854a948e
Taejin Kim <rlaxowls1316@naver.com>
2024-05-04 09:35:02 +0900

    Fix: SearchShopPage mobile height issue

--- src/components/SearchShopPage/SearchShopPage.tsx
+++ src/components/SearchShopPage/SearchShopPage.tsx
@@ -13,23 +13,25 @@ export default function SearchShopPage({
 }: SearchShopPageProps) {
   return (
     <PageModal isOpen={isOpen}>
-      <div className={styles.container}>
-        <div className={styles.searchContainer}>
-          <img width={18} src={searchIcon} alt="search_icon" />
-          <input className={styles.search} type="text" placeholder="검색" />
-        </div>
-
-        <div className={styles.mapArea}>
-          this is for map
-          <div className={styles.selectedBox}>
-            <h2 className={styles.selectedBoxTitle}>선택된 가게 없음</h2>
-            <p className={styles.selectedBoxAddress}>가게를 선택해주세요.</p>
+      <div className={styles.test}>
+        <div className={styles.container}>
+          <div className={styles.searchContainer}>
+            <img width={18} src={searchIcon} alt="search_icon" />
+            <input className={styles.search} type="text" placeholder="검색" />
           </div>
-        </div>
 
-        <div className={styles.buttonsContainer}>
-          <button onClick={onClose}>취소</button>
-          <button>완료</button>
+          <div className={styles.mapArea}>
+            this is for map
+            <div className={styles.selectedBox}>
+              <h2 className={styles.selectedBoxTitle}>선택된 가게 없음</h2>
+              <p className={styles.selectedBoxAddress}>가게를 선택해주세요.</p>
+            </div>
+          </div>
+
+          <div className={styles.buttonsContainer}>
+            <button onClick={onClose}>취소</button>
+            <button>완료</button>
+          </div>
         </div>
       </div>
     </PageModal>
