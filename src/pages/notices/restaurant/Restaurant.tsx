09434991432aa5908e0c0039e27910cb6dc497c6
Taejin Kim <rlaxowls1316@naver.com>
2024-06-01 20:35:53 +0900

    fix: adjust lineHeight

--- src/pages/notices/restaurant/Restaurant.tsx
+++ src/pages/notices/restaurant/Restaurant.tsx
@@ -70,7 +70,7 @@ export default function Restaurant() {
               당일 예약 가능 & 빠른 주문 제작
             </h2>
 
-            <p style={{ marginBottom: "3rem" }}>
+            <p style={{ marginBottom: "3rem", lineHeight: "1.5" }}>
               갑작스럽게 케이크를 준비해야할 때 걱정하지 마세요! 초초로에는 당일
               예약이 가능하답니다. 컵케이크는 10분~30분 전에, 케이크는
               30분~2시간 전에 연락 주시면 신속하면서도 정성스럽게
@@ -90,7 +90,7 @@ export default function Restaurant() {
           <section style={{ marginTop: "8rem" }} className={styles.section}>
             <h2 className={styles.sectionTitle}>착한 가격의 1인 케이크</h2>
 
-            <p style={{ marginBottom: "3rem" }}>
+            <p style={{ marginBottom: "3rem", lineHeight: "1.5" }}>
               요즘은 1만원대 케이크 찾기 어려우시죠? 초초로에서는 정성스럽게
               제작한 수제 케이크를 합리적인 가격으로 만나볼 수 있습니다. 특별한
               날, 특별한 순간을 초초로와 함께 하세요! 🍰✨
@@ -105,11 +105,13 @@ export default function Restaurant() {
               </div>
             </div>
 
-            <p style={{ marginTop: "3rem" }}>
+            <p style={{ marginTop: "3rem", lineHeight: "1.5" }}>
               사장님이 말씀하시기를, 망고 디저트가 특히 인기가 많다고 해요!
               신선한 망고의 풍부한 맛이 가득한 디저트들을 종류별로 즐겨보세요.
-              🥭💛 초초로에서 달콤한 시간을 함께 하세요. 여러분의 방문을
-              기다리고 있겠습니다!
+              🥭💛 <br />
+              <br />
+              초초로에서 달콤한 시간을 함께 하세요. 여러분의 방문을 기다리고
+              있겠습니다!
             </p>
           </section>
         </div>
