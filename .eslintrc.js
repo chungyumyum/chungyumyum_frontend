1f255e95872538b6c1be7b1106e9074163c310c2
Taejin Kim <rlaxowls1316@naver.com>
2024-06-28 23:02:51 +0900

    chore: eslint 파일 수정

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+modolue.exports = {
+    "extends": [
+      "eslint:recommended",
+      "plugin:react/recommended",
+      "plugin:@typescript-eslint/recommended"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "plugins": ["react", "@typescript-eslint"],
+    "settings": {
+      "react": {
+        "version": "detect" // 사용자가 설치한 버전을 자동으로 선택
+      }
+    },
+    "rules": {
+      "react/react-in-jsx-scope": "off"
+    }
+  }
